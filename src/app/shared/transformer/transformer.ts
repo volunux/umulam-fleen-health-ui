4fb0bb066c9d3356508187ca03b6fb258a7cef41
Yusuf Alamu Musa <volunux@gmail.com>
2023-08-18 22:15:36 +0100

    add null check before trasnforming data

--- src/app/shared/transformer/transformer.ts
+++ src/app/shared/transformer/transformer.ts
@@ -1,21 +1,25 @@
+import {isObject} from "../util/helpers";
 
 export function toRequestBody(data: any): Record<string, string> {
   const newData: Record<string, string> = {};
 
-  for (const property of Object.keys(data)) {
-    const transformedProperty = property.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase();
-    newData[transformedProperty] = data[property];
+  if (isObject(data)) {
+    for (const property of Object.keys(data)) {
+      const transformedProperty: string = property.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase();
+      newData[transformedProperty] = data[property];
+    }
   }
-
   return newData;
 }
 
 export function toBody(data: any): string {
   const newData: Record<string, string> = {};
 
-  for (const property of Object.keys(data)) {
-    const transformedProperty: string = property.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase();
-    newData[transformedProperty] = data[property];
+  if (isObject(data)) {
+    for (const property of Object.keys(data)) {
+      const transformedProperty: string = property.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase();
+      newData[transformedProperty] = data[property];
+    }
   }
 
   return toJson(newData);
