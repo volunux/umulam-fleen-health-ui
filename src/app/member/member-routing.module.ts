35048e7d40ce137c06a6216bb4bc66c8bac9b0da
Yusuf Alamu Musa <volunux@gmail.com>
2023-08-31 23:30:05 +0100

    add robots.txt

--- src/app/member/member-routing.module.ts
+++ src/app/member/member-routing.module.ts
@@ -5,6 +5,9 @@ import {MemberBaseComponent} from "./component/member-base/member-base.component
 import {MemberDashboardComponent} from "./component/member-dashboard/member-dashboard.component";
 import {MemberDetailComponent} from "./component/member-detail/member-detail.component";
 import {MemberUpdateComponent} from "./component/member-update/member-update.component";
+import {
+  MemberUpdateEmailPhoneComponent
+} from "./component/member-update-email-phone/member-update-email-phone.component";
 
 const routes: Routes = [
   { path: '',
@@ -14,7 +17,8 @@ const routes: Routes = [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', component: MemberDashboardComponent, title: 'Profile Dashboard' },
       { path: 'get-detail', component: MemberDetailComponent, title: 'Profile Detail' },
-      { path: 'update-detail', component: MemberUpdateComponent, title: 'Profile Update' }
+      { path: 'update-detail', component: MemberUpdateComponent, title: 'Profile Update' },
+      { path: 'update-email-phone', component: MemberUpdateEmailPhoneComponent, title: 'Update Email & Phone' }
     ]
   }
 ];
