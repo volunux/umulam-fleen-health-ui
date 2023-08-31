a9b93fbf375b43a43d0db8d7486883767c7263c0
Yusuf Alamu Musa <volunux@gmail.com>
2023-08-31 12:51:12 +0100

    allow user to retrieve details for update

--- src/app/member/service/member.service.ts
+++ src/app/member/service/member.service.ts
@@ -18,6 +18,14 @@ export class MemberService {
     return this.httpService.get(req)
       .pipe(
         map(data => new GetMemberUpdateDetailsResponse(data))
-      )
+      );
+  }
+
+  public getUpdateDetails(): Observable<GetMemberUpdateDetailsResponse> {
+    const req: BaseRequest = this.httpService.toRequest([this.BASE_PATH, 'update-details']);
+    return this.httpService.get(req)
+      .pipe(
+        map(data => new GetMemberUpdateDetailsResponse(data))
+      );
   }
 }
