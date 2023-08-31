a5cc85ea4a9b9fb076adabe27edad4b67a9f2923
Yusuf Alamu Musa <volunux@gmail.com>
2023-08-31 15:27:08 +0100

    add component basic details for retrieving member details

--- src/app/member/component/member-detail/member-detail.component.ts
+++ src/app/member/component/member-detail/member-detail.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GetMemberUpdateDetailsResponse} from "../../response/get-member-update-details.response";
+import {FormBuilder} from "@angular/forms";
+import {Observable} from "rxjs";
+import {MemberService} from "../../service/member.service";
+import {Router} from "@angular/router";
+import {ErrorResponse} from "../../../base/response/error-response";
 
 @Component({
   selector: 'app-member-detail',
   templateUrl: './member-detail.component.html',
   styleUrls: ['./member-detail.component.css']
 })
-export class MemberDetailComponent {
+export class MemberDetailComponent implements OnInit {
+  protected entryView!: GetMemberUpdateDetailsResponse;
+
+  public constructor(protected memberService: MemberService,
+                     protected router: Router) { }
+
+  public ngOnInit(): void {
+    this.memberService.getDetails()
+      .subscribe({
+        next: (result: GetMemberUpdateDetailsResponse): void => {
+          this.entryView = result;
+        },
+        error: (error: ErrorResponse): void => {
+
+        }
+      });
+  }
 
 }
