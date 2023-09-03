dfee3d960ba902603f0237c60c60a84f3d52c8d4
Yusuf Alamu Musa <volunux@gmail.com>
2023-09-03 14:15:34 +0100

    add typing to status text

--- src/app/shared/util/file-upload-download-messages.ts
+++ src/app/shared/util/file-upload-download-messages.ts
@@ -5,12 +5,12 @@ export const statusText: StatusText = {
   'timeout': 'Request has timed out',
   'filesize': {
     'valid' : 'File size is valid and file can be uploaded.',
-    'invalid' : (size, label = 'MB') => {
+    'invalid' : (size: number, label: string = 'MB'): string => {
       return `File is too large. File size should be less than equal to ${size}${label}`;
     }
   },
   'filetype': {
-    'invalid' : (allowedTypes) => {
+    'invalid' : (allowedTypes: string[]): string => {
       return `File type is invalid and file cannot be uploaded. Allowed types : ${allowedTypes.join(',')}`
     },
     'valid' : 'File type is valid and file can be uploaded.'
@@ -25,8 +25,8 @@ export const statusText: StatusText = {
     'error': 'An error has occurred. Please try again.',
     'abort': 'File upload cancelled.',
     'timeout': 'File upload timeout. Please try again.',
-    'inProgress': (percentage) => {
-      return `Uploading file: ${percentage}%`;
+    'inProgress': (percentage: number | string): string => {
+      return `Uploading file: ${percentage.toString()}%`;
     }
   },
   'deleteObject': {
@@ -35,7 +35,7 @@ export const statusText: StatusText = {
     'error': 'An error has occurred. Please try again.'
   },
   'fileTypeAllowedToUploadText' : 'Only Image Files or Documents with file extensions like docx or pdf is allowed in this field.',
-  'fileChosenText' : (fileName) => {
+  'fileChosenText' : (fileName: string): string => {
     fileName = fileName && fileName.trim() !== ''
       ? fileName
       : 'Unknown';
