502565563f5ff67e7b1b257191daaffc20453c51
Yusuf Alamu Musa <volunux@gmail.com>
2023-09-04 22:37:28 +0100

    enable logout of user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component } from '@angular/core';
-
-import * as Stomp from 'stompjs';
-import * as SockJS from 'sockjs-client';
-import $ from 'jquery';
+import {Component} from '@angular/core';
+import {AuthenticationService} from "./authentication/service/authentication.service";
+import {BASE_PATH} from "./shared/constant/base-config";
+import {Router} from "@angular/router";
 
 
 @Component({
@@ -11,4 +10,25 @@ import $ from 'jquery';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  public constructor(protected authenticationService: AuthenticationService,
+                     protected router: Router) { }
+
+  get isAuthenticated(): boolean {
+    return this.authenticationService.isAuthenticationStatusCompleted();
+  }
+
+  public async logout(): Promise<void> {
+    this.authenticationService.clearAuthTokens();
+    await this.goHome();
+  }
+
+  protected async goHome(): Promise<void> {
+    await this.getRouter().navigate([BASE_PATH]);
+  }
+
+  protected getRouter(): Router {
+    return this.router;
+  }
+
 }
