b8ed1f36019fdae269c19a9fffca95a57fc6482a
Yusuf Alamu Musa <volunux@gmail.com>
2023-09-16 21:31:56 +0100

    add base implementation for view professionals component

--- src/app/health-session/component/view-professionals/view-professionals.component.ts
+++ src/app/health-session/component/view-professionals/view-professionals.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BaseEntriesComponent} from "../../../base/component/base-entries/base-entries.component";
+import {ProfessionalView} from "../../../professional/view/professional.view";
+import {SearchFilter} from "../../../shared/type/search";
+import {SEARCH_FILTER_VIEW_PROFESSIONALS} from "../../../shared/constant/search-filter";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Location} from "@angular/common";
+import {HealthSessionService} from "../../service/health-session.service";
+import {AnyProp} from "../../../shared/type/base";
+import {Observable, of} from "rxjs";
+import {SearchResultView} from "../../../shared/view/search-result.view";
+import {DeleteIdsDto} from "../../../shared/type/other";
+import {DeleteResponse} from "../../../shared/response/delete.response";
+import {ANY_EMPTY} from "../../../shared/constant/other-constant";
 
 @Component({
   selector: 'app-view-professionals',
   templateUrl: './view-professionals.component.html',
   styleUrls: ['./view-professionals.component.css']
 })
-export class ViewProfessionalsComponent {
+export class ViewProfessionalsComponent extends BaseEntriesComponent<ProfessionalView> implements OnInit {
+
+  public override entries: ProfessionalView[] = [];
+  public override searchFilter: SearchFilter[] = SEARCH_FILTER_VIEW_PROFESSIONALS;
+
+  public constructor(protected healthSessionService: HealthSessionService,
+                     router: Router,
+                     route: ActivatedRoute, location: Location) {
+    super(router, route, location);
+  }
+
+  public ngOnInit(): void {
+    this.startComponent();
+  }
+
+  override findEntries(params: AnyProp): Observable<SearchResultView<ProfessionalView>> {
+    return this.healthSessionService.viewProfessionals(params);
+  }
+
+  override deleteEntries(dto: DeleteIdsDto): Observable<DeleteResponse> {
+    return of(ANY_EMPTY);
+  }
 
 }
--- src/app/shared/constant/search-filter.ts
+++ src/app/shared/constant/search-filter.ts
@@ -22,3 +22,16 @@ export const SEARCH_FILTER_BETWEEN_DATE: SearchFilter[] = [
   {key: NO_INPUT_KEY, label: ''},
   {key: BETWEEN_DATE_SEARCH_KEY, type: BETWEEN_DATE_TYPE, label: BETWEEN_DATE_SEARCH_LABEL },
 ];
+
+
+export const SEARCH_FILTER_VIEW_PROFESSIONALS: SearchFilter[] = [
+  ...SEARCH_FILTER_BETWEEN_DATE,
+  {key: 'firstName', label: 'First Name'},
+  {key: 'lastName', label: 'Last Name' },
+  {key: 'emailAddress', label: 'Email Address' },
+  {key: 'verificationStatus', label: 'Verification Status' },
+  {key: 'availabilityStatus', label: 'Availability Status'},
+  {key: 'professionalType', label: 'Professional Type'},
+  {key: 'professionalQualificationType', label: 'Qualification Type'},
+  {key: 'languageSpoken', label: 'Language Spoken'}
+];
